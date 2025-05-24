52f86e7d4586e9071435ef15deb576e90e918e05
Saad Sajid Akram <saadakram21@gmail.com>
2025-05-24 15:32:14 +0100

    Up to Chapter 12

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
+  experimental: {
+    ppr: 'incremental'
+  }
 };
 
 export default nextConfig;
